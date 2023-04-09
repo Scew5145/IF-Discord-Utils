3af99661ccdbb3c8f5ae4b7a5948a6e52dada7cf
Scott Ewing <Scew5145@colorado.edu>
2023-04-08 20:41:53 -0600

    Only ping online users instead of literally everyone

--- FeedbackBot/main.py
+++ FeedbackBot/main.py
@@ -10,9 +10,10 @@ TOKEN = os.environ["DISCORD_TOKEN"]
 GUILD_ID = int(os.environ["DISCORD_GUILD_ID"])
 ROLE_ID = int(os.environ["DISCORD_ROLE"])
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.messages = True
 intents.members = True
+intents.presences = True
 
 
 class FeebasClient(discord.Client):
@@ -33,17 +34,32 @@ tree = app_commands.CommandTree(feebas)
 
 @tree.command(guild=discord.Object(id=GUILD_ID), description="tag 5 random members with the feedback giver role")
 async def feedbackpls(interaction: discord.Interaction):
-    guild = feebas.get_guild(GUILD_ID)
+    guild = interaction.guild
     role = get(guild.roles, id=ROLE_ID)
     sample_count = 5
     sample = []
-    if sample_count <= len(role.members):
-        sample = random.sample(role.members, sample_count)
+    allowed_ping_statuses = [discord.Status.online, discord.Status.idle, discord.Status.dnd]
+    ids = [member.id for member in role.members]
+
+    # splitting requests for the sake of the API call
+    split_ids = list(chunk_array(ids, 100))
+    feedback_users = []
+    for group in split_ids:
+        feedback_users += (await guild.query_members(user_ids=group, presences=True))
+    online_users = [member for member in feedback_users if member.status in allowed_ping_statuses]
+    if sample_count <= len(online_users):
+        sample = random.sample(online_users, sample_count)
     else:
-        sample = role.members
+        sample = online_users
     tags = [f"<@{member.id}>" for member in sample]
+    # tags = ["bacon"]
     joined_tags = '\n'.join(tags)
     await interaction.response.send_message(f"YOU HAVE BEEN SUMMONED:\n{joined_tags}", ephemeral=False)
+
+
+def chunk_array(in_list, n):
+    for i in range(0, len(in_list), n):
+        yield in_list[i:i + n]
 
 feebas.run(TOKEN)
 
