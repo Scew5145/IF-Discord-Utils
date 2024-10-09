dfc433af68dcf96979e4a3a3959c259d7fde7dac
Scott Ewing <x.xirel.x@gmail.com>
2024-10-08 19:06:06 -0600

    remove var print

--- FeedbackBot/main.py
+++ FeedbackBot/main.py
@@ -10,7 +10,6 @@ import os
 from typing import AsyncIterator
 
 # from config import TOKEN, GUILD_ID, ROLE_ID
-print(os.environ)
 TOKEN = os.environ["DISCORD_TOKEN"]
 GUILD_ID = int(os.environ["DISCORD_GUILD_ID"])
 ROLE_ID = int(os.environ["DISCORD_ROLE"])
